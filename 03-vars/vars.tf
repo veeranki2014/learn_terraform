19a24cfd4f9071f65f6561950804007206f3acfd
veeranki2014 <veeranki2014@gmail.com>
2023-06-25 20:09:05 -0500

    Ansible Robo Setup

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -15,5 +15,46 @@ output "sample01" {
 
 ## if sometime if any variable/reference combination of someother stings, we can access using ${} flower braces.
 output "sample_ext" {
-  value = "testing variable in double quotes ${var.sample01}"
+  value = "testing variable in double quotes ${var.sample01 }"
 }
+
+##Data Types in Terraform variables
+# Strings
+# Numbers
+# Boolean
+
+## Variable types in Ansible 1) plain key=value 2) List 3) Map/Dictionary
+## Variable Types in Terraform
+# 1. Plain
+# 2. List
+# 3. Map
+
+#plain:
+variable "course" {
+  default = "DevOps"
+}
+
+## List
+variable "courses" {
+  default = [
+    "DevOps",
+    "AWS",
+    "AZURE"
+  ]
+}
+
+## Map
+variable "course_details" {
+  default = {
+    DevOps = {
+      course = "DevOps",
+      Timings = "9PM",
+      duration = 90
+    }
+    AWS = {
+      course = "aws",
+      duration = 30
+    }
+  }
+}
+
