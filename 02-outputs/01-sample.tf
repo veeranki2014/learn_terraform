e705962fbed2a08538cd4b08105f38bcfba36c68
veeranki2014 <veeranki2014@gmail.com>
2023-06-25 19:07:14 -0500

    Ansible Robo Setup

---
+++ 02-outputs/01-sample.tf
@@ -0,0 +1,9 @@
+output "sample" {
+  value = "Hello Terraform"
+}
+##Any string in terraform shoul be in double quotes.""
+## Number no need of double or single quotes.
+
+output "sample01" {
+  value = 1000
+}
