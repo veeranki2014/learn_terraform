541a27c518e2d44be2ae424be5cbba214b34ed38
veeranki2014 <veeranki2014@gmail.com>
2023-06-26 19:57:12 -0500

    Ansible Robo Setup

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -1,73 +1,77 @@
-variable "sample" {
-  default = "Hello World"
+#variable "sample" {
+#  default = "Hello World"
+#}
+#variable "sample01" {
+#  default = 1000
+#}
+#
+#output "sample" {
+#  value = var.sample
+#}
+#
+#output "sample01" {
+#  value = var.sample01
+#}
+#
+### if sometime if any variable/reference combination of someother stings, we can access using ${} flower braces.
+#output "sample_ext" {
+#  value = "testing variable in double quotes ${var.sample01 }"
+#}
+#
+###Data Types in Terraform variables
+## Strings
+## Numbers
+## Boolean
+#
+### Variable types in Ansible 1) plain key=value 2) List 3) Map/Dictionary
+### Variable Types in Terraform
+## 1. Plain
+## 2. List
+## 3. Map
+#
+##plain:
+#variable "course" {
+#  default = "DevOps"
+#}
+#
+### List
+#variable "courses" {
+#  default = [
+#    "DevOps",
+#    "AWS",
+#    "AZURE"
+#  ]
+#}
+#
+### Map
+#variable "course_details" {
+#  default = {
+#    DevOps = {
+#      course = "DevOps",
+#      Timings = "9PM",
+#      duration = 90
+#    }
+#    AWS = {
+#      course = "aws",
+#      duration = 30
+#    }
+#  }
+#}
+#
+### How to access variables in terraform
+#output "course" {
+#  value = var.course
+#}
+#
+#output "courses" {
+#  value = var.courses[2]
+#}
+#
+#output "course_details" {
+#  value = var.course_details["AWS"]
+#}
+#
+variable "env" {}
+output "dev_prod" {
+  value = var.env
 }
-variable "sample01" {
-  default = 1000
-}
-
-output "sample" {
-  value = var.sample
-}
-
-output "sample01" {
-  value = var.sample01
-}
-
-## if sometime if any variable/reference combination of someother stings, we can access using ${} flower braces.
-output "sample_ext" {
-  value = "testing variable in double quotes ${var.sample01 }"
-}
-
-##Data Types in Terraform variables
-# Strings
-# Numbers
-# Boolean
-
-## Variable types in Ansible 1) plain key=value 2) List 3) Map/Dictionary
-## Variable Types in Terraform
-# 1. Plain
-# 2. List
-# 3. Map
-
-#plain:
-variable "course" {
-  default = "DevOps"
-}
-
-## List
-variable "courses" {
-  default = [
-    "DevOps",
-    "AWS",
-    "AZURE"
-  ]
-}
-
-## Map
-variable "course_details" {
-  default = {
-    DevOps = {
-      course = "DevOps",
-      Timings = "9PM",
-      duration = 90
-    }
-    AWS = {
-      course = "aws",
-      duration = 30
-    }
-  }
-}
-
-## How to access variables in terrrafrom
-output "course" {
-  value = var.course
-}
-
-output "courses" {
-  value = var.courses[2]
-}
-
-output "course_details" {
-  value = var.course_details["AWS"].[1]
-}
-
