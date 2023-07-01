e7ce28495afadbd6b7e28ffcdeff20e618f05cce
veeranki2014 <veeranki2014@gmail.com>
2023-07-01 14:39:04 -0500

    Terraform Fundamentals

--- 07-modules-1/ec2/main.tf
+++ 07-modules-1/ec2/main.tf
@@ -1,10 +1,12 @@
 resource "aws_instance" "main" {
   instance_type = "t2.micro"
-  ami = ""
+  ami = data.aws_ami.main.id
+  vpc_security_group_ids = [ aws_security_group.main.id]
 
   tags = {
-    name = "sample"
+    Name = "sample"
   }
+
 }
 
 data "aws_ami" "main"{
@@ -13,4 +15,28 @@ data "aws_ami" "main"{
   most_recent = true
 }
 
-resource "aws_security_group" "main" {}
+resource "aws_security_group" "main" {
+  name        = "sample"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "sample"
+  }
+}
