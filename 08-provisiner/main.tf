cf4ad162d50a14a15ff30aef47caef0c91422f47
veeranki2014 <veeranki2014@gmail.com>
2023-07-02 14:00:52 -0500

    Terraform Fundamentals

--- 08-provisiner/main.tf
+++ 08-provisiner/main.tf
@@ -1,21 +1,22 @@
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.main.id
-  instance_type = "t2.micro"
+  ami                       = data.aws_ami.main.id
+  instance_type             = "t2.micro"
+  vpc_security_group_ids    = "sg-00cd0cb49be555ef2"
 
-  tags = {
-    Name = "HelloWorld"
+  tags                      = {
+    Name                    = "HelloWorld"
   }
 
   provisioner "remote-exec" {
 
     connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
-      host     = self.public_ip
+      type                  = "ssh"
+      user                  = "centos"
+      password              = "DevOps321"
+      host                  = self.public_ip
     }
 
-    inline = [
+    inline                  = [
       "sudo labauto ansible",
       "ansible-pull -i localhost, -U https://github/com/veeranki2014/roboshop_ansible main.yml -e env=dev -e role_name=frontend"
     ]
@@ -25,7 +26,7 @@ resource "aws_instance" "web" {
 
 
 data "aws_ami" "main"{
-  owners = ["973714476881"]
-  most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
+  owners                  = ["973714476881"]
+  most_recent             = true
+  name_regex              = "Centos-8-DevOps-Practice"
 }
