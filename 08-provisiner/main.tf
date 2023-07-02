0f569265c1435778cd0492607dbf019fab6b0591
veeranki2014 <veeranki2014@gmail.com>
2023-07-02 14:15:00 -0500

    Terraform Fundamentals

--- 08-provisiner/main.tf
+++ 08-provisiner/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "web" {
 
     inline                  = [
       "sudo labauto ansible",
-      "ansible-pull -i localhost, -U https://github/com/veeranki2014/roboshop_ansible main.yml -e env=dev -e role_name=frontend"
+      "ansible-pull -i localhost, -U https://github.com/veeranki2014/roboshop_ansible main.yml -e env=dev -e role_name=frontend"
     ]
 
   }
