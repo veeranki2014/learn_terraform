14a327a6efded74025083faf2661389148301083
veeranki2014 <veeranki2014@gmail.com>
2023-07-11 18:11:17 -0500

    Terraform Fundamentals

--- 11-funtions/main.tf
+++ 11-funtions/main.tf
@@ -6,3 +6,11 @@ variable "class" {
 output "class" {
   value = upper(var.class)
 }
+
+variable "fruits" {
+  default = "apple, orange, banana"
+}
+
+output "fruits_names" {
+  value = var.fruits
+}
