0c602e9286f634564762707f98db1926cf473ce8
veeranki2014 <veeranki2014@gmail.com>
2023-07-12 15:59:05 -0500

    Terraform Fundamentals

---
+++ 10-loops/for-each/main.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "web" {
+  for_each                  = var.instances
+  ami                       = data.aws_ami.main.id
+  instance_type             = "t2.micro"
+  #count                     = length(var.instances)
+  tags                      = {
+    Name                    = element(var.instances,count.index )
+  }
+}
+
+data "aws_ami" "main"{
+  owners                    = ["973714476881"]
+  most_recent               = true
+  name_regex                = "Centos-8-DevOps-Practice"
+}
+
+variable "instances" {
+  default = {
+    frontend = {
+      name          = "frontend"
+      instance_type = "t3.micro"
+    }
+    catalogue = {
+      name          = "catalogue"
+      instance_type = "t3.nano"
+    }
+    cart = {
+      name          = "cart"
+      instance_type = "t3.small"
+    }
+  }
+}
