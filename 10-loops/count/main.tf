bfd9de6a233f714f844be1354206a800c8dd542e
veeranki2014 <veeranki2014@gmail.com>
2023-07-12 15:26:48 -0500

    Terraform Fundamentals

--- 10-loops/count/main.tf
+++ 10-loops/count/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   instance_type             = "t2.micro"
   count                     = length(var.instances)
   tags                      = {
-    Name                    = "HelloWorld"
+    Name                    = "Helloworld - ${count.index}"
   }
 }
 
